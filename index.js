8276e6fb266fa182bb3bda13683ade368721b2f7
fuad <ahmadfuadoorakam@gmail.com>
2024-08-14 17:38:49 +0530

    added files

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('1');
+const fs = require('fs')
+
+const userList = [
+    { name: 'fuad' },
+    { name: 'aju' },
+    { name: 'saju' },
+    { name: 'basi' },
+]
+
+fs.writeFileSync('file2.txt', 'this is file two')
+fs.writeFileSync('userList.json', JSON.stringify(userList))
+
+const data = fs.readFileSync('file2.txt', 'utf-8')
+const isExist = fs.existsSync('file2.txt')
+
+
+console.log('writing completed');
+console.log(isExist);
+console.log(data);
+
