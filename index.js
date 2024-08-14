258aacf283d539945249dba01bc46161b3d3fc3e
fuad <ahmadfuadoorakam@gmail.com>
2024-08-14 19:27:53 +0530

    some

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require('console')
 const fs = require('fs')
 
 const userList = [
@@ -10,19 +11,25 @@ const userList = [
 
 // fs.writeFileSync('file2.txt', 'this is file two')
 // fs.writeFileSync('userList.json', JSON.stringify(userList, undefined, 4))
-// fs.appendFileSync('file4.txt', 'this is user requist \n')
+// fs.appendFileSync('file43.txt', 'this is user requist \n', (err) => {
+//     if (err) throw err
+//     console.log('data');
+// })
 // fs.unlinkSync('file2.txt')
 // fs.mkdir('api')
 
-fs.writeFile('file4.txt', 'this plain page', (err) => {
-    if (err) throw err
+// fs.readFile('file4.txt', 'utf-8', (err, data) => {
+//     if (err) throw err
+//     console.log(data);
+// })
 
-    console.log('success');
+fs.access('file4.txt', (err) => {
+    if (err) throw err
+    console.log("success");
+    
 })
 
-
-
-// const data = fs.readFileSync('userList.json', 'utf-8')
+// const data = fsnode index.readFileSync('userList.json', 'utf-8')
 // const isExist = fs.existsSync('file2.txt')
 
 
