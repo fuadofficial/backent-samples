159f1becc3da47600d7f4caf1503242b42911933
fuad <ahmadfuadoorakam@gmail.com>
2024-08-14 20:31:23 +0530

    created server swetup

--- http.js
+++ http.js
@@ -1,8 +1,22 @@
 const http = require('http');
+const { parse } = require('url');
 
-http.createServer((req, res) => {
-    res.write('hi');
-    res.end();
-}).listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const userList = [
+    { name: "fuad" },
+    { name: "aju" },
+    { name: "saju" },
+    { name: "basi" },
+
+]
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(userList));
+})
+
+const PORT = 3000
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
