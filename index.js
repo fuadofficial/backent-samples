728398992332c9ff4641208df72bff91ee7e7a76
fuad <ahmadfuadoorakam@gmail.com>
2024-08-17 13:44:31 +0530

    added post method

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 const movieList = require('./movie.json')
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json(movieList)
 })
+app.post('/', (req, res) => {
+    console.log(req.body);
 
+    res.json(movieList)
+})
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`server running on ${PORT}`))
+const PORT = 3000;
+app.listen(PORT, () => console.log(`server running on ${PORT}`)) 
