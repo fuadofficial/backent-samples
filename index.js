522a298b948f4c4a3c989bb2336be0701e2f124c
fuad <ahmadfuadoorakam@gmail.com>
2024-08-17 13:57:05 +0530

    some changes for post

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
+    console.log(req.query);
+    
     res.json(movieList)
 })
 app.post('/', (req, res) => {
     console.log(req.body);
-
     res.json(movieList)
 })
 
