6ba384bf7b4492cb9b9238c85f3c5f6debeb3670
fuad <ahmadfuadoorakam@gmail.com>
2024-08-18 20:35:55 +0530

    some

--- index.js
+++ index.js
@@ -6,17 +6,37 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/api/movies', (req, res) => {
     const { movieName } = req.query;
-    const filterMovies = movieList.filter(movie => movie.title.toLocaleLowerCase().includes(movieName.toLocaleLowerCase()))
+    let filterMovies = []
+    if (movieName) {
+        const filterMovies = movieList.filter(movie => movie.title.toLocaleLowerCase().includes(movieName.toLocaleLowerCase()))
+    } else {
+        filterMovies = movieList
+    }
     res.json({
         results: filterMovies
     })
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.json(movieList)
+app.post('/api/movies', (req, res) => {
+    const { movieName, movieId } = req.body
+    movieList.push({
+        id: movieId,
+        title: movieName,
+        poster_path: '',
+    })
+    res.json({
+        results: movieList
+    })
+})
+
+app.delete('/api/movies', (req, res) => {
+    const { movieName } = req.body
+    const fileredMovieList = movieList.filter(movie => movie.title.toLocaleLowerCase() !== movieName.toLocaleLowerCase())
+    res.json({
+        results: fileredMovieList
+    })
 })
 
 const PORT = 3000
