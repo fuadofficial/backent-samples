615649004543b6d9c6cecbf2ed9b9a1b8f942c8d
fuad <ahmadfuadoorakam@gmail.com>
2024-08-23 21:13:39 +0530

    created sample midleware

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const movieList = require('./movie.json')
 
+app.use(cors())
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log('inside the midleware.......');
+    const { auth } = req.body
+    if (auth) {
+        return next()
+    }
+    console.log("user not login . please login");
+
+})
 
 app.get('/', (req, res) => {
-    res.json(movieList)
+    res.json({
+        message: "hy"
+    })
+})
+app.post('/', (req, res) => {
+    return res.json({
+        message: "success"
+    })
+
 })
 
 
