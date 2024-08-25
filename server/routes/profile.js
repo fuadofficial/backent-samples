502c1a4b6c9d74be31f73dedf8fe05286906cc9a
unknown <ahmadfuadoorakam@gmail.com>
2024-08-25 09:02:23 -0700

    added file some

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -21,7 +21,7 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/upload", upload.single, (req, res) => {
+router.post("/upload", upload.single('upload_file'), (req, res) => {
     console.log(req.body);
     console.log(req.file);
 
