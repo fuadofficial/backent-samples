b2b960da4ac35f1c766e4f9026e89f08abeba13d
unknown <ahmadfuadoorakam@gmail.com>
2024-08-25 22:04:33 -0700

    added file to server and fix error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+
+const API_URL = 'http://localhost:5000/api/profile/upload'
 
 const App = () => {
-    const [image, setImage] = useState('')
+    const [image, setImage] = useState(null)
 
     const handleImage = (event) => {
-        setImage(URL.createObjectURL(event.target.files[0]))
+        setImage(event.target.files[0])
     }
 
-    const uploadImage = () => {
+    const uploadImage = async () => {
+        const formData = new FormData()
 
+        formData.append('upload_files', image)
+        formData.append('firstName', 'fuad')
+
+        try {
+            const response = await axios(API_URL, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "mulipart/form-data",
+                },
+                data: formData
+            })
+            console.log(response.data)
+        } catch (error) {
+            console.error("Error uploading image:", error)
+        }
     }
 
     return (
         <div>
-            <img src={image} alt="" style={{ width: '200px', height: 'auto' }} />
             <input type="file" accept='image/*' onChange={handleImage} />
+            {image && <img src={URL.createObjectURL(image)} alt="Preview" style={{ width: '200px', height: 'auto' }} />}
             <button onClick={uploadImage}>Upload</button>
         </div>
     )
